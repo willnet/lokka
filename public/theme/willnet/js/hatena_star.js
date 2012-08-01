41e38da8034628b154e3f25f0433d5b266b51b4d
willnet <netwillnet@gmail.com>
2012-08-01 19:02:19 +0900

    Refactor

---
+++ public/theme/willnet/js/hatena_star.js
@@ -0,0 +1,10 @@
+Hatena.Star.Token = '1509a63676578c07e2c82ee86c136049078c6565';
+Hatena.Star.SiteConfig = {
+  entryNodes: {
+    'article': {
+      uri: 'h2 a',
+      title: 'h2',
+      container: 'h2'
+    }
+  }
+};
