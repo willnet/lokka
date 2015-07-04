0f07f9708426c93421ebe017cfc0f23f48911a9d
willnet <netwillnet@gmail.com>
2015-07-04 10:32:05 +0900

    Revert "Add assets"
    This reverts commit 5db01a56353da97db5234decd2b127bb5ffa0bef.

---
+++ public/theme/willnet/js/hatena_star.js
@@ -0,0 +1,10 @@
+Hatena.Star.Token = '1509a63676578c07e2c82ee86c136049078c6565';
+Hatena.Star.SiteConfig = {
+  entryNodes: {
+    'article': {
+      uri: 'h1 a',
+      title: 'h1',
+      container: 'h1'
+    }
+  }
+};
